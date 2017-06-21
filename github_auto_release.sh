cc04a14bb50af995604564a1885e6d984db3a735
Patrick Conway <pconway@pcm-pconway-1.local>
2017-06-21 18:18:07 -0400

    changing curl statement

--- github_auto_release.sh
+++ github_auto_release.sh
@@ -128,10 +128,11 @@ if [[ "$RELEASE_VERSION" =~ [0-9]+[.][0-9]+[.][0-9]+ ]] && [[ "$RELEASE_VERSION"
 			OS_TYPE=$(echo "$OS_TYPE" | tr '[:upper:]' '[:lower:]') #convert OSTYPE to lower case
 			if [[ "$OS_TYPE" == "darwin" ]] || [[ "$OS_TYPE" == "linux" ]]; then
 				echo " --- Getting necessary github-release executable from github.com/aktau/github-release --- "
-				curl -OL "https://github.com/aktau/github-release/releases/download/$EXECUTABLE_VERSION/$OS_TYPE-amd64-github-release.tar.bz2" 
-				tar -zxvf "$OS_TYPE"-amd64-github-release.tar.bz2
-				mv bin/"$OS_TYPE"/amd64/github-release $EXECUTABLE_PATH
-				rm -R "$OS_TYPE"-amd64-github-release.tar.bz2 bin
+				#curl -OL "https://github.com/aktau/github-release/releases/download/$EXECUTABLE_VERSION/$OS_TYPE-amd64-github-release.tar.bz2" 
+				curl "https://github.com/aktau/github-release/releases/download/$EXECUTABLE_VERSION/$OS_TYPE-amd64-github-release.tar.bz2" > $EXECUTABLE_PATH
+				tar -zxvf "$EXECUTABLE_PATH/$OS_TYPE"-amd64-github-release.tar.bz2
+				#mv bin/"$OS_TYPE"/amd64/github-release $EXECUTABLE_PATH
+				rm -R "$EXECUTABLE_PATH/$OS_TYPE"-amd64-github-release.tar.bz2 bin
 				echo " --- github-release executable now located in $EXECUTABLE_PATH --- "
 			elif [[ "$OS_TYPE" == "mingw" ]]; then #haven't tested on windows
 				curl -OL "https://github.com/aktau/github-release/releases/download/v0.7.2/windows-amd64-github-release.zip" 
