275d735b13ba6b6a0bb121728b061fcd8d04c56d
Dana Maxfield <danam@synopsys.com>
2021-09-19 21:50:40 -0400

    One source for help/usage. Organized and space consistently

--- github_auto_release.sh
+++ github_auto_release.sh
@@ -5,24 +5,32 @@
 ## Purpose: Automatically release build artifacts to GitHub on stable, non-SNAPSHOT, project builds. Uses the following project: https://github.com/aktau/github-release. 
 ##
 ## How to: to run, ./github_auto_release.sh <parameters>     
-## Parameters:
-##      -b|--buildTool 						conditionally required: specify build tool (maven or gradle for now) if you are not providing a releaseVersion.
-##      -v|--releaseVersion                 conditionally required: specify the release version of the project.
-##		-o|--owner		   					required: the name of the owner under which the repo is located (default is blackducksoftware)
-##		-f|--artifactFile        			optional: specify file path to project's artifact file (if build artifact is not standard, user can specify to make sure it is released) <CANNOT SPECIFY BOTH A DIRECTORY AND FILE>
-##		-t|--artifactType					conditionally optional <if specified, artifactDirectory must also be specified>: if file artifact file type is not .zip, .tar, or .jar, specify a file type here and the script will look for a file in workspace that follows the convention of REPO_NAME-RELEASE_VERSION with specified ending
-##												*you may define as .jar to look for file with name repo_name-release_version.jar, or define regex.jar to find a jar file that matches given regex (ex. *.jar matches any jar with any name)
-##		-d|--artifactDirectory 				conditionally optional <if specified, artifactType must also be specified>: specify a directory to be zipped and released <CANNOT SPECIFY BOTH A DIRECTORY AND FILE>
-##												*note that if there are more multiple directories with subdirectories of the same name, then you must specify assembly/target, or similar
-##												*define file paths as follows sub1/sub2/sub3 ...
-##		-p|--project						conditionally required: IF using a NuGet project, you must provide a project name
-##		-n|--attachArtifacts 				optional: choice to override attaching binaries. If set to false, script will only tag non-SNAPSHOT versions.
-##		-m|--releaseDesc         			optional: add description for release to github
-##		-ev|--executableVersion   			optional: which version of the GitHub-Release executable to be used (default is v0.10.0 because that is the version this script is being tested with)
-##		-ep|--executablePath 	   			optional: where on the user's machine the GitHub-Release executable will live (defualt is set to ~/temp/blackducksoftware)
-## 		-h|--help 							help menu
-##
-################################################################################################################################################################################################ 
+################################################################################################################################################################################################
+
+_usage_="
+Parameters:
+  -d|--artifactDirectory    conditionally optional <if specified, artifactType must also be specified>: specify a directory to be zipped and released
+                              <CANNOT SPECIFY BOTH -d|--artifactDirectory AND -f|--artifactFile>
+                              *note that if there are more multiple directories with subdirectories of the same name, then you must specify assembly/target, or similar
+                              *define file paths as follows sub1/sub2/sub3 ...
+  -t|--artifactType         conditionally optional <if specified, artifactDirectory must also be specified>: if file artifact file type is not .zip, .tar, or .jar,
+                              specify a file type here and the script will look for a file in workspace that follows the convention of REPO_NAME-RELEASE_VERSION
+                              with specified ending
+                              *you may define as .jar to look for file with name repo_name-release_version.jar, or define regex.jar to find a jar file that matche
+                              given regex (ex. *.jar matches any jar with any name)
+  -f|--artifactFile         optional: specify file path to project's artifact file (if build artifact is not standard, user can specify to make sure it is released)
+                              <CANNOT SPECIFY BOTH -d|--artifactDirectory AND -f|--artifactFile>
+  -n|--attachArtifacts      optional: choice to override attaching binaries. If set to false, script will only tag non-SNAPSHOT versions.
+  -b|--buildTool            conditionally required: specify build tool (maven or gradle for now) if you are not providing a releaseVersion.
+  -m|--releaseDesc          optional: add description for release to github
+  -ep|--executablePath      optional: where on the user's machine the GitHub-Release executable will live (default is set to ~/temp/GARTool)
+  -ev|--executableVersion   optional: which version of the GitHub-Release executable to be used (default is v0.10.0)
+  -p|--nugetProject         conditionally required: IF using a NuGet project, you must provide a project name
+  -o|--owner                required: the name of the owner under which the repo is located
+  -v|--releaseVersion       conditionally required: specify the release version of the project.
+  -br|--branch              optional: the branch that should be tagged on release
+  -h|--help                 help menu
+"
 
 ARTIFACT_DIRECTORY=""
 ARTIFACT_FILE=""
@@ -98,17 +106,7 @@ do
 			echo "	- Github-release target branch: $TARGET"
 			;;
         -h|--help) 
-            echo "HELP MENU - options"
-			echo "-b|--buildTool 					required: specify build tool (maven, gradle, or nuget)"
-			echo "-f|--artifactFile        			optional: specify the exact file path to project's artifact file"
-			echo "-d|--artifactDirectory 			conditionally optional <if specified, artifactType must also be specified>: specify a directory to be zipped and released <CANNOT SPECIFY BOTH A DIRECTORY AND FILE>"
-			echo "-t|--artifactType 				conditionally optional <if specified, artifactDirectory must also be specified>: specify a file type for script to find, or specify a regex for a file name. (ex. '.jar' will find repo_name-release_version.jar, OR '*repo-name*.jar' will find files matching regex) "
-			echo "-n|--attachArtifacts 				optional: choice to override attaching binaries. If set to false, script will only tag non-SNAPSHOT versions."
-			echo "-m|--releaseDesc         			optional: add description for release to github (deaflt is 'Github Autorelease')" 
-			echo "-o|--owner		   				optional: the name of the owner under which the repo is located (default is blackducksoftware)"
-			echo "-ev|--executableVersion   		optional: which version of the GitHub-Release executable to be used (default is v0.10.0)"
-			echo "-ep|--executablePath 	   			optional: where on the user's machine the GitHub-Release executable will live (defualt is ~/temp/blackducksoftware)"
-			echo "-br|--branch 	   					optional: the branch that should be tagged on release"
+            echo "${_usage_}"
 			exit 1
 			;;
 		*)
